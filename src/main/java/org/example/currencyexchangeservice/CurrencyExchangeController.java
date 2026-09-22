87a862971ec3e19660275b4e486114da879a711c
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-09-22 20:40:59 +0700

    v110: Using JPA Repository to retrieve data from H2 database

--- src/main/java/org/example/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/org/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -12,15 +12,24 @@ import java.math.BigDecimal;
 public class CurrencyExchangeController {
 
     private final Environment environment;
-    public CurrencyExchangeController(Environment environment) {
+    private final CurrencyExchangeRepository repository;
+    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository repository) {
         this.environment = environment;
+        this.repository = repository;
     }
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to) {
 
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        //CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null) {
+
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
+
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
 
---
+++ src/main/java/org/example/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package org.example.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
