5895c4d39b487cbb4eac71e07d7c81f7ac53b468
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-09-22 11:13:15 +0700

    v105: Setting up Dynamic port in the response

--- src/main/java/org/example/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/org/example/currencyexchangeservice/CurrencyExchange.java
@@ -9,6 +9,16 @@ public class CurrencyExchange {
     private String to;
     private BigDecimal conversionMultiple;
 
+    private String environment;
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
     public CurrencyExchange() {
     }
 
