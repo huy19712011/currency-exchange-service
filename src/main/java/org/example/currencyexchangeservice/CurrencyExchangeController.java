1a9272264db636be45c02b2a4823ac308509a6b0
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-09-24 11:29:03 +0700

    v156: Connecting Currency Exchange Microservice with Zipkin

--- src/main/java/org/example/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/org/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package org.example.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     private final Environment environment;
     private final CurrencyExchangeRepository repository;
     public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository repository) {
@@ -21,6 +25,8 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to) {
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
 
         //CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
